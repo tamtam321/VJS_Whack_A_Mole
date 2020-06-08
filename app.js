59d27ad826c474be495e7c2ac3b9cbc9c3a715bb
Nguyen Thien Tam <tamtam132@gmail.com>
2020-06-08 19:08:40 +0200

    Made html, css, js. Made html basic structure, css style and have started app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+const square = document.querySelectorAll(".square");
+const mole = document.querySelectorAll(".mole");
+const timeLeft = document.querySelector("#time-left");
+const score = document.querySelector("#score");
+
