f23ceb0b395a5cbc48ec57219ecf944383b844de
Nguyen Thien Tam <tamtam132@gmail.com>
2020-06-08 21:20:18 +0200

    I have started app.js. Added some variables and have started do randomSquare function.

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const square = document.querySelectorAll(".square");
 const mole = document.querySelectorAll(".mole");
 const timeLeft = document.querySelector("#time-left");
-const score = document.querySelector("#score");
+let score = document.querySelector("#score");
 
+let result = 0;
+
+function randomSquare()
+{
+    square.forEach(className =>
+    {
+        className.classList.remove("mole");
+    });
+
+    let randomPosition = square[Math.floor(Math.random() * 9)];
+    randomPosition.classList.add("mole");
+}
